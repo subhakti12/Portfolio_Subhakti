cb89853c5a1205e1bf71d56c9690ecb5f5f1b563
subhakti12 <2210631170028@student.unsika.ac.id>
2025-01-12 00:03:29 +0700

    pembaharuan 5

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         secondary: '#64748b',
         dark: '#0f172a',
         hitam: '#030712',
+        Blue: '#1d4ed8',
+        Rose: '#4c0519',
+        Blue1: '#38bdf8',
+        Slate: '#334155',
       },
       screens: {
         '2xl': '1320px'
